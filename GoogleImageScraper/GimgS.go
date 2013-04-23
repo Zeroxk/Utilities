ceffb725113e51e77c4668e77d51b6068eb96d84
dhu009 <zeroxk123@gmail.com>
2013-04-23 12:26:03 +0200

    Keyword Golang in GOport

--- GoogleImageScraper/GimgS.go
+++ GoogleImageScraper/GimgS.go
@@ -44,8 +44,9 @@ func readURL(url string) []byte {
 
 func main() {
 
-	var keyword string
-	fmt.Scanf("%s", &keyword)
+/*	var keyword string
+    fmt.Scanf("%s", &keyword)*/
+keyword := "Golang"
 	fmt.Println("Your keyword is:",keyword,"\n")
 	
     query := "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&q=" + keyword
