63af08e61d29ab771bae2925b27cff61b6ce9d0b
Zeroxk <dhu009@student.uib.no>
2013-04-27 03:59:44 +0200

    Starting CD is now 30s, max CD 30min, doesnt change working dir but writes to specified dir

--- 4chanImageScraper/4chImgScr.go
+++ 4chanImageScraper/4chImgScr.go
@@ -13,12 +13,18 @@ import (
 	"bufio"
 )
 
+const (
+		MAX_COOLDOWN = 1800
+		DEFAULT_COOLDOWN = 30
+)
+
 type Thread struct {
     Posts    []*Post
     Board    string
     Time_rcv string
 	Cooldown int64
 	LastPost int
+	Dir string
 }
 
 type Post struct {
@@ -62,8 +68,8 @@ func parseJSON(jsonObj []byte, t *Thread) {
 
 }
 
-//Download thread images
-func downloadImages(posts []*Post, board string) {
+//Download thread images into specified directory
+func downloadImages(posts []*Post, board, dir string) {
 
 	fmt.Println("Starting image downloads")
     for _, p := range(posts) {
@@ -74,8 +80,9 @@ func downloadImages(posts []*Post, board string) {
 			fmt.Println("Downloading image from", url)
 
 			img,_ := readURL(url)
-
-			err := ioutil.WriteFile((strconv.FormatInt(p.Tim, 10) + p.Ext), img, 0755)
+			
+			path := strings.Join( []string{dir, `\`, strconv.FormatInt(p.Tim, 10), p.Ext}, "" )
+			err := ioutil.WriteFile(path, img, 0755)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -84,6 +91,7 @@ func downloadImages(posts []*Post, board string) {
         }
 
     }
+	
 	fmt.Println("Done downloading images from thread\n")
 
 }
@@ -96,7 +104,7 @@ func get_Thread(url string) (thread *Thread, json string) {
 	thread = new(Thread)	
 	thread.Board = tmp[3]
 	
-	thread.Cooldown = 10
+	thread.Cooldown = DEFAULT_COOLDOWN
 	
 	json = strings.Join( []string{"http://api.4chan.org/", strings.Join(tmp[3:], "/"), ".json"}, "" )
 	
@@ -131,7 +139,7 @@ func update(json string, thread *Thread) {
 
 	fmt.Println("Thread last modified:", thread.Time_rcv)
 
-	downloadImages(postsDelta, thread.Board)
+	downloadImages(postsDelta, thread.Board, thread.Dir)
 	
 	thread.LastPost = t.LastPost
 	fmt.Println("Last post is:", strconv.FormatInt(thread.Posts[thread.LastPost].No, 10), "\n")
@@ -156,22 +164,17 @@ func main() {
 	
 	//fmt.Println(dir)
 	
-	err := os.Chdir(dir)
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-	
 	fmt.Println("Url is:", url)
-	fmt.Println("Changed directory to:", dir, "\n")
+	fmt.Println("Directory is:", dir, "\n")
 	
 	thread, json := get_Thread(url)
+	thread.Dir = dir
 	
-    downloadImages(thread.Posts, thread.Board)
+    downloadImages(thread.Posts, thread.Board, thread.Dir)
 		
 	for {
 	
-		fmt.Println("Sleeping")
+		fmt.Println("Sleeping for", thread.Cooldown, "seconds")
 		time.Sleep(time.Duration(thread.Cooldown)*time.Second)
 		fmt.Println("Woke up")
 	
@@ -186,19 +189,23 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-	
+		
+		fmt.Println("Status code for request response:", r.StatusCode)
 		switch sc := r.StatusCode; sc {
-			case 404: fmt.Println("Thread died at time: ", time.Now().Format("Sun, 03 Feb 1991 17:30:02 GMT"))
+			case 404: fmt.Println("Thread died at time: ", time.Now().UTC())
 						os.Exit(0)
 			
-			case 304: fmt.Println("Nothing new"); thread.Cooldown *=2
+			case 304: fmt.Println("Nothing new\n")
+						if tc := thread.Cooldown*2; tc > MAX_COOLDOWN{
+							thread.Cooldown = MAX_COOLDOWN
+						}else {
+							thread.Cooldown = tc
+						}
 			
 			default: fmt.Println("Thread has been updated")
-					 thread.Cooldown = 10			
+					 thread.Cooldown = DEFAULT_COOLDOWN			
 					 update(json, thread)
 		}
-
-		fmt.Println("Cooldown is:", thread.Cooldown, "\n")
 	
 	}
 	
