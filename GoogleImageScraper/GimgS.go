990c91d8351c0937443f293ecb9bbd4e2d75440a
Zeroxk <dhu009@student.uib.no>
2013-05-01 20:23:48 +0200

    Now has parallell download support, downloads multiples of 4 images

--- GoogleImageScraper/GimgS.go
+++ GoogleImageScraper/GimgS.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "io/ioutil"
-        "encoding/json"
-		"strings"
-        "os"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
+    "strings"
+    "sync"
 )
 
-const dir = "imgsGo" 
+const dir = "imgsGo\\"
 
 type RespData struct {
     ResponseData Results
 }
 
 type Results struct {
-	Results []Image
+    Results []Image
 }
 
 type Image struct {
@@ -25,68 +27,86 @@ type Image struct {
 }
 
 func readURL(url string) []byte {
-	resp, err := http.Get(url);
-    
-    if(err != nil) {
+    resp, err := http.Get(url)
+
+    if err != nil {
         log.Fatal(err)
     }
 
     body, err := ioutil.ReadAll(resp.Body)
-	
-	if(err != nil) {
+
+    defer resp.Body.Close()
+    if err != nil {
         log.Fatal(err)
-		resp.Body.Close();
     }
-	defer resp.Body.Close();
-	
-	return body
+
+    return body
 }
 
 func main() {
 
-	var keyword string
-    fmt.Scanf("%s", &keyword)
-	fmt.Println("Your keyword is:",keyword,"\n")
-	
-    query := "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&q=" + keyword
-	fmt.Println(query)
-	
-    contents := readURL(query)
+    var keyword string
+    var numDls int
 
-	fmt.Println("Parsing JSON")
-    var rpD RespData
-    err := json.Unmarshal(contents, &rpD)
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Done parsing JSON\n")
-	
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		os.Mkdir(dir, 0755)
-	}
-	
-	fmt.Println("Changing working directory to", dir)
-	err = os.Chdir(dir)
-	
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	fmt.Println("Starting image download")
-	
-	res := rpD.ResponseData.Results
-	for _,img := range(res) {
-		name := img.Url[ (strings.LastIndex(img.Url, "/")) + 1:]
-		fmt.Println(name)
-		image := readURL(img.Url)
-		
-		err = ioutil.WriteFile(name, image, 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	
-	fmt.Println("Finished")
+    fmt.Print("Keyword:")
+    fmt.Scanf("%s\n", &keyword)
+
+    fmt.Print("# of images to dl:")
+    fmt.Scanf("%d\n\n", &numDls)
+
+    fmt.Println("Your keyword is:", keyword)
+    fmt.Println("Downloading", strconv.Itoa(numDls), "images\n")
+
+    var wg sync.WaitGroup
+
+    for i := 0; i < numDls; i += 4 {
+
+        wg.Add(1)
+
+        start := strconv.Itoa(i)
+
+        go func(start, keyword string) {
+
+            query := "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&start=" + start + "&q=" + keyword
+            fmt.Println(query)
+
+            contents := readURL(query)
+
+            fmt.Println("Parsing JSON")
+
+            rpD := new(RespData)
+            err := json.Unmarshal(contents, &rpD)
+
+            if err != nil {
+                log.Fatal(err)
+            }
+            fmt.Println("Done parsing JSON\n")
+
+            if _, err := os.Stat(dir); os.IsNotExist(err) {
+                os.Mkdir(dir, 0755)
+            }
+
+            fmt.Println("Starting image download")
+
+            res := rpD.ResponseData.Results
+            for _, img := range res {
+
+                name := img.Url[(strings.LastIndex(img.Url, "/"))+1:]
+                fmt.Println(name)
+                image := readURL(img.Url)
+
+                err = ioutil.WriteFile((dir + name), image, 0755)
+                if err != nil {
+                    log.Fatal(err)
+                }
+            }
+            wg.Done()
+
+        }(start, keyword)
+
+    }
+
+    wg.Wait()
+
+    fmt.Println("Finished")
 }
-
